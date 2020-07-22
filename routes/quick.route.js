7a4b531295ce5b6477e5cd51e9935c20317fb3ef
Isabella Chew <izzycsy@gmail.com>
2020-07-22 21:45:29 +0800

    cloudinary

--- routes/quick.route.js
+++ routes/quick.route.js
@@ -3,30 +3,31 @@ const Quick = require("../models/quick.model");
 // const moment = require("moment");
 const cloudinary = require("cloudinary");
 const multer = require("multer");
-const upload = multer({ dest: "public/img/" });
+const upload = multer({ dest: "./uploads/" });
 const isLoggedIn = require("../config/loginBlocker");
 
 router.get("/create", (req, res) => {
     res.render("quick/create");
 });
 
-router.post("/create", upload.single("upload"), isLoggedIn, (req, res) => {
+router.post("/create", upload.single("image"), isLoggedIn, (req, res) => {
     console.log(req.user);
     req.body.createdBy = req.user._id;
     
-    console.log(req.upload);
+    // console.log(req.upload);
     cloudinary.uploader.upload(req.file.path, function (result) {
         console.log(result);
-        req.body.img = result.url;
-    });
+        req.body.image = result.url;
 
-    let quick = new Quick(req.body);
-    quick.save().then(() => {
+        let quick = new Quick(req.body);
+        quick.save().then(() => {
             req.flash("success", "Quick & Simple created");
             res.redirect("/dashboard"); //should redirect to "list of Quick" recipes
         }).catch((err) => {
             console.log(err);
         });
+    });
+
     //add other categories
 });
 
