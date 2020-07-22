4bc03a8dd40c88ba03665d54ab27420b92487ce6
Isabella Chew <izzycsy@gmail.com>
2020-07-22 23:11:21 +0800

    quick/showAll

--- routes/quick.route.js
+++ routes/quick.route.js
@@ -6,6 +6,7 @@ const multer = require("multer");
 const upload = multer({ dest: "./uploads/" });
 const isLoggedIn = require("../config/loginBlocker");
 
+//CREATE FORM
 router.get("/create", (req, res) => {
     res.render("quick/create");
 });
@@ -22,7 +23,7 @@ router.post("/create", upload.single("image"), isLoggedIn, (req, res) => {
         let quick = new Quick(req.body);
         quick.save().then(() => {
             req.flash("success", "Quick & Simple created");
-            res.redirect("/dashboard"); //should redirect to "list of Quick" recipes
+            res.redirect("/quick/index"); //after creating redirect to "list of Quick" recipes
         }).catch((err) => {
             console.log(err);
         });
@@ -30,5 +31,23 @@ router.post("/create", upload.single("image"), isLoggedIn, (req, res) => {
 
     //add other categories
 });
+
+//VIEW CREATED FORMS
+router.get("/index", (req, res) => {
+    Quick.find()
+    .populate("createdBy")
+    .then((quicks) => {
+        // console.log("createdBy", quicks[0].createdBy);
+        res.render("quick/index", { quicks });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+
+//SHOW INDIVIDUAL FORM
+router.get("/show/:id", (req, res) => {
+    res.send("show page");
+})
 
 module.exports = router;
